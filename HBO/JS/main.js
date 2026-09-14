66b4d24d3770e3d95a80904440df0973d065f13d
Christian <118530836+faze-chris@users.noreply.github.com>
2026-09-14 14:53:18 +0200

    added who am i and projects

--- HBO/JS/main.js
+++ HBO/JS/main.js
@@ -1,4 +1,6 @@
 // alert("testing js");
+
+// assignment 1, 2 & 3 lesson 1,2 & 3
 const output = document.getElementById("output");
 const h1 = document.querySelector(".h1");
 const p = document.querySelector(".p");
@@ -34,7 +36,7 @@ const producten = [
     populariteit: 10,
   },
 ];
-    for (let i = 0; i < producten.length; i++) {
+for (let i = 0; i < producten.length; i++) {
   const product = producten[i];
   const productElement = document.createElement("div");
   productElement.innerHTML =
@@ -48,29 +50,28 @@ const producten = [
   output.appendChild(productElement);
 }
 function updateOutput(producten) {
-    for (let i = 0; i < producten.length; i++) {
-  const product = producten[i];
-  const productElement = document.createElement("div");
-  productElement.innerHTML =
-    "<h2>" +
-    product.naam +
-    "</h2><p>Prijs: " +
-    product.prijs +
-    "</p><p>Populariteit: " +
-    product.populariteit +
-    "</p>";
-  output.appendChild(productElement);
-}
+  for (let i = 0; i < producten.length; i++) {
+    const product = producten[i];
+    const productElement = document.createElement("div");
+    productElement.innerHTML =
+      "<h2>" +
+      product.naam +
+      "</h2><p>Prijs: " +
+      product.prijs +
+      "</p><p>Populariteit: " +
+      product.populariteit +
+      "</p>";
+    output.appendChild(productElement);
+  }
 }
 function handleClick1() {
   producten.sort((a, b) => a.prijs - b.prijs);
-    updateOutput(producten);
+  updateOutput(producten);
 }
 function handleClick2() {
   producten.sort((a, b) => b.populariteit - a.populariteit);
   updateOutput(producten);
 }
-
 
 btn1.addEventListener("click", handleClick1);
 btn2.addEventListener("click", handleClick2);
