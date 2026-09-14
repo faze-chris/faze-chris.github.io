8dbd7aa654d7d82be27e44184f4b43286db0285b
Christian <118530836+faze-chris@users.noreply.github.com>
2026-09-14 10:42:46 +0200

    les 3 opdracht done js

--- HBO/JS/main.js
+++ HBO/JS/main.js
@@ -1,1 +1,11 @@
 // alert("testing js");
+const output = document.getElementById("output");
+const h1 = document.querySelector(".h1");
+const p = document.querySelector(".p");
+
+h1.textContent = "Christian Lemmen";
+h1.innerHTML = "<b>Lemmen</b>";
+p.innerHTML = "lol";
+const newElement = document.createElement("p");
+newElement.innerHTML = "Hello, World!";
+output.appendChild(newElement);
